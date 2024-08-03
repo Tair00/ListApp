8c4848eaf83544c7d2410e4698899126db1d39f8
Tair00 <suleimanov.t.i.20@gmail.com>
2024-08-03 11:46:24 +0300

    working with a fragment

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.shoppinglist.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
@@ -12,7 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
     private lateinit var viewModel: MainViewModel
     private lateinit var shopListAdapter: ShopListAdapter
     private var shopItemContainer: FragmentContainerView? = null
@@ -32,9 +33,14 @@ class MainActivity : AppCompatActivity() {
             val intent = ShopItemActivity.newIntentAddItem(this)
             startActivity(intent)
             }else{
+
                 launchFragment(ShopItemFragment.newInstanceAddItem())
             }
         }
+    }
+   override fun onEditingFinished(){
+       Toast.makeText(this@MainActivity,"Success",Toast.LENGTH_SHORT).show()
+       supportFragmentManager.popBackStack()
     }
     private fun isOnePaneMode(): Boolean{
         return shopItemContainer == null
