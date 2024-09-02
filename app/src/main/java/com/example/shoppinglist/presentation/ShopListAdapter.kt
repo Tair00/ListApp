b0655a641462d138e4d9d3c9e084b4f9a26d1b65
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-02 08:47:02 +0300

    database

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ItemShopDisabledBinding
+import com.example.shoppinglist.databinding.ItemShopEnabledBinding
 import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
@@ -11,52 +13,55 @@ class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCa
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-
-        val layout = when(viewType){
+        val layout = when (viewType) {
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
-            else -> throw RuntimeException("Unknow view type: $viewType")
+            else -> throw RuntimeException("Unknown view type: $viewType")
         }
-        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
-        return ShopItemViewHolder(view)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            LayoutInflater.from(parent.context),
+            layout,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
-
         val shopItem = getItem(position)
-        viewHolder.tvName.text = shopItem.name
-        viewHolder.tvCount.text = shopItem.count.toString()
-
-        viewHolder.view.setOnLongClickListener {
-           onShopItemLongClickListener?.invoke(shopItem)
+        val binding = viewHolder.binding
+        binding.root.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-        viewHolder.view.setOnClickListener{
+        binding.root.setOnClickListener {
             onShopItemClickListener?.invoke(shopItem)
         }
-
+        when (binding) {
+            is ItemShopDisabledBinding -> {
+                binding.shopItem = shopItem
+            }
+            is ItemShopEnabledBinding -> {
+                binding.shopItem = shopItem
+            }
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        val item =  getItem(position)
-        return if (item.enabled){
+        val item = getItem(position)
+        return if (item.enabled) {
             VIEW_TYPE_ENABLED
-        }else{
+        } else {
             VIEW_TYPE_DISABLED
         }
     }
 
+    companion object {
 
-
-
-
-    companion object{
         const val VIEW_TYPE_ENABLED = 100
         const val VIEW_TYPE_DISABLED = 101
-        const val MAX_POOL_SIZE = 25
+
+        const val MAX_POOL_SIZE = 30
     }
-
-
 }
--- app/src/main/java/com/example/shoppinglist/presentation/ShopListDiffCallback.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListDiffCallback.kt
@@ -3,16 +3,17 @@ package com.example.shoppinglist.presentation
 import androidx.recyclerview.widget.DiffUtil
 import com.example.shoppinglist.domain.ShopItem
 
-class ShopListDiffCallback (
+class ShopListDiffCallback(
     private val oldList: List<ShopItem>,
     private val newList: List<ShopItem>,
-    ):DiffUtil.Callback(){
-    override fun getNewListSize(): Int {
-        return  newList.size
-    }
+): DiffUtil.Callback() {
 
     override fun getOldListSize(): Int {
-        return  oldList.size
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
     }
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
@@ -20,10 +21,10 @@ class ShopListDiffCallback (
         val newItem = newList[newItemPosition]
         return oldItem.id == newItem.id
     }
+
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
-        return oldItem.id == newItem.id
+        return oldItem == newItem
     }
-
 }
