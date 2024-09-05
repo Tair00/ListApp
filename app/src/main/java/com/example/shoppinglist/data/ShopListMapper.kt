d85ba491ad1d05a1ebe0b0b1c53fee5c1063e5de
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-05 17:06:08 +0300

    Mapper

--- app/src/main/java/com/example/shoppinglist/data/ShopListMapper.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListMapper.kt
@@ -1,4 +1,14 @@
 package com.example.shoppinglist.data
 
+import com.example.shoppinglist.domain.ShopItem
+
 class ShopListMapper {
+    fun mapEntityToDbModel(shopItem: ShopItem):ShopItemDbModel{
+        return ShopItemDbModel(
+            id = shopItem.id,
+            name = shopItem.name,
+            count = shopItem.count,
+            enabled = shopItem.enabled
+        )
+    }
 }
