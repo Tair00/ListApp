965e64a91097bb6e892619e45f073289ed9be88b
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-05 17:02:16 +0300

    DataBinding

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,22 +1,15 @@
 package com.example.shoppinglist.data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import kotlin.random.Random
 
-object ShopListRepositoryImpl: ShopListRepository {
+class ShopListRepositoryImpl(application: Application): ShopListRepository {
     private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
-    private var autoIncrementId = 0
-    private val shopListLD = MutableLiveData<List<ShopItem>>()
-    init {
-        for (i in 0 until 10)
-        {
-            val item = ShopItem( "Name $i", i, Random.nextBoolean())
-            addShopItem(item)
-        }
-    }
+
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.add(shopItem)
     }
