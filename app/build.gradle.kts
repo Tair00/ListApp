e7a5fd28e5a0cfac367638ca450bfd810295505e
Tair00 <suleimanov.t.i.20@gmail.com>
2024-09-08 12:00:30 +0300

    MediatorLiveData

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,47 +36,30 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
 dependencies {
-    implementation("androidx.databinding:databinding-common:8.6.0")
-    implementation("androidx.databinding:databinding-runtime:8.6.0")
-    val nav_version = "2.7.7"
-
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-
-    // Feature module Support
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
-
-    // Testing Navigation
-    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
-
-    // Jetpack Compose Integration
-    implementation("androidx.navigation:navigation-compose:$nav_version")
-
-    // Core libraries
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
 
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
-
-    // Lifecycle components
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-
-    implementation ("androidx.room:room-runtime:2.3.0")
-    implementation ("androidx.room:room-ktx:2.3.0")
-
-
-    // Testing
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1") // Поддержка LiveData и Transformations
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5") // ViewModel
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("com.google.dagger:dagger:2.35.1")
+    implementation("androidx.databinding:databinding-common:8.6.0")
+    kapt("com.google.dagger:dagger-compiler:2.35.1")
+    kapt("androidx.room:room-compiler:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
 }
--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,41 +1,34 @@
 package com.example.shoppinglist.data
 
-import android.app.Application
+
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
+
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
-import kotlin.random.Random
+import androidx.lifecycle.*
+import javax.inject.Inject
 
-class ShopListRepositoryImpl(application: Application): ShopListRepository {
-    private val shopListDao = AppDatabase.getInstance(application).shopListDao()
-    private val mapper =ShopListMapper()
-    override fun deleteShopItem(shopItem: ShopItem) {
-        shopListDao.deleteShopItem(shopItem.id)
-    }
+class ShopListRepositoryImpl @Inject constructor(
+    private val shopListDao: ShopListDao,
+    private val mapper: ShopListMapper
+) : ShopListRepository {
 
-    override fun addShopItem(shopItem: ShopItem) {
-       shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
-    }
-
-    override fun editShopItem(shopItem: ShopItem){
+    override suspend fun addShopItem(shopItem: ShopItem) {
         shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun getShopItem(shopItemId: Int): ShopItem {
-       val dbModel = shopListDao.getShopItem(shopItemId)
+    override suspend fun deleteShopItem(shopItem: ShopItem) {
+        shopListDao.deleteShopItem(shopItem.id)
+    }
+
+    override suspend fun editShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
+    }
+
+    override suspend fun getShopItem(shopItemId: Int): ShopItem {
+        val dbModel = shopListDao.getShopItem(shopItemId)
         return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopList(): LiveData<List<ShopItem>> = MediatorLiveData<List<ShopItem>>().apply {
-        addSource(shopListDao.getShopList()){
-            if(it.size>10){
-                value = mapper.mapListDbModelToListEntity(it)
-            }
-        }
-    }
-
-
-
+    override fun getShopList(): LiveData<List<ShopItem>> = Transformation.
 }
